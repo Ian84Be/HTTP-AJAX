0e336c38b57b87b9e762f744424c71f2ec249b0f
Ian Be <ian84be@gmail.com>
2019-02-27 17:40:28 -0600

    fix add & delete functions

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
-import './App.scss';
-
 import axios from 'axios';
+
+import './App.scss';
 import FriendCard from './components/FriendCard';
 import FriendForm from './components/FriendForm';
 
 export default class App extends Component {
   state = {
     friends: [],
+    name:'',
+    age:'',
+    email:'',
   };
 
   componentDidMount() {
     axios
       .get('http://localhost:5000/friends')
       .then(response => {
-        console.log(response.data);
+        this.setState({
+          friends: response.data
+        })
+      })
+      .catch(err => console.log(err));
+  }
+
+  addFriend = e => {
+    e.preventDefault();
+    const newFriend = {
+      age: this.state.age,
+      email: this.state.email,
+      name: this.state.name,
+    }
+    axios
+      .post('http://localhost:5000/friends', newFriend)
+      .then(response => {
         this.setState({
           friends: response.data
         })
@@ -23,19 +42,43 @@ export default class App extends Component {
   }
 
   deleteFriend = e => {
-    // e.preventDefault();
-    console.log(e.target.value);
     axios
       .delete(`http://localhost:5000/friends/${e.target.value}`)
-      .then(res => console.log(res.data));
-    window.location.reload();
+      .then(res => this.setState({
+        friends: res.data
+      }))
+      .catch(err => console.log(err));
+  }
+
+  handleChange = e => {
+    e.preventDefault();
+    this.setState({
+        [e.target.name]: e.target.value
+    });
+  }
+
+  resetForm = e => {
+    e.preventDefault();
+    this.setState({
+      name:'',
+      age:'',
+      email:'',
+    });
   }
 
   render() {
     return (
       <div className="App">
         <h1>myFriends</h1>
-        <FriendForm />
+        <FriendForm 
+          addFriend={this.addFriend}
+          age={this.state.age}
+          email={this.state.email}
+          handleChange={this.handleChange}
+          name={this.state.name} 
+          onChange={this.handleChange}
+          resetForm={this.resetForm}
+          />
         <div className="friend-container">
         {this.state.friends.map(friend => {
           return (
--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -1,81 +1,36 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './FriendForm.scss';
-import axios from 'axios';
 
-class FriendForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            name:'',
-            age:'',
-            email:'',
-         }
-    }
+const FriendForm = props => {
+    return (  
+        <form className="FriendForm" onSubmit={props.addFriend}>
+            <input onChange={props.handleChange}
+                name="name"
+                type="text"
+                placeholder="name"
+                value={props.name}
+            required />
+                
+            <input onChange={props.handleChange}
+                name="age"
+                type="text"
+                placeholder="age"
+                value={props.age}
+            required />
 
-    handleChange = e => {
-        e.preventDefault();
-        // if (e.target.type === 'checkbox') {
-        //     return (
-        //         this.setState({ [e.target.name]: e.target.checked })
-        //     )
-        // } else 
-        this.setState({
-            [e.target.name]: e.target.value
-        });
-    }
+            <input onChange={props.handleChange}
+                name="email"
+                type="text"
+                placeholder="email"
+                value={props.email}
+            required />
 
-    onSubmit = e => {
-        // e.preventDefault();
-        console.log(this.state);
-        axios
-            .post('http://localhost:5000/friends', this.state)
-            .then(res => {
-                console.log(res);
-                console.log(res.data);
-              });
-    }
-    
-    resetForm = e => {
-        e.preventDefault();
-        this.setState({
-            name:'',
-            age:'',
-            email:'',
-        });
-    }
+            <div className="FriendForm__buttons">
+            <input type="submit" value="Submit" />
+            <input type="reset" onClick={props.resetForm}/>
+            </div>
 
-    render() { 
-        return ( 
-            <form className="FriendForm" onSubmit={this.onSubmit}>
-                <input onChange={this.handleChange}
-                    name="name"
-                    type="text"
-                    placeholder="name"
-                    value={this.state.name}
-                required />
-                    
-                <input onChange={this.handleChange}
-                    name="age"
-                    type="text"
-                    placeholder="age"
-                    value={this.state.age}
-                required />
-
-                <input onChange={this.handleChange}
-                    name="email"
-                    type="text"
-                    placeholder="email"
-                    value={this.state.email}
-                required />
-
-                <div className="FriendForm__buttons">
-                <input type="submit" value="Submit" />
-                <input type="reset" onClick={this.resetForm}/>
-                </div>
-
-            </form>
-         );
-    }
+        </form>
+    );
 }
- 
 export default FriendForm;
